06c4ab71e9d0da5d9711d749be750baa7ecad101
moloch291 <danielkomjati1@gmail.com>
2020-07-25 22:44:19 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# main functions will be writing and reading alongside creating note saving and possibly notification functions
+import notes
+
+
+def main():
+    notes.pyary()
+
+
+if __name__ == '__main__':
+    main()
---
+++ notes.py
@@ -0,0 +1,15 @@
+import functions
+
+
+def options_menu():
+    input("Welcome to 'Notes', the python diary!\n1: Create notes\n2: Read notes\n3: Exit")
+
+
+
+def menu():
+    functions.show_calendar()
+    options_menu()
+
+
+def notes():
+    menu()
