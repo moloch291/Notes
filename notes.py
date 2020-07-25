bbf80b7d4c4f550031c06d2ee1731864d6c84085
moloch291 <danielkomjati1@gmail.com>
2020-07-26 01:49:22 +0200

    Note creation added alongside the sketch of a 'read only' option. Console clearing feature added.

--- notes.py
+++ notes.py
@@ -7,23 +7,27 @@ def create_new_note():
     current_time = functions.get_current_time()
     file_name = functions.name_file()
     note_name = functions.generate_note_name(current_time, file_name)
+    os.system('clear')
     time.sleep(1)
     note = functions.create_note(note_name)
     print(file_name + ":")
     message = input("New note (type 'cancel' to exit): ")
     if message == "cancel":
         options_menu()
-    note = functions.write_note(note, message)
-    functions.save_and_close_note(note)
-    options_menu()
+    else:
+        note = functions.write_note(note, message)
+        functions.save_and_close_note(note)
+        options_menu()
 
 
 def read_existing_notes():
-    print("Choose a note to read!")
+    print("Under development")
+    time.sleep(1)
     options_menu()
 
 
 def options_menu():
+    os.system('clear')
     menu_choice = input("Welcome to 'Notes', the python diary!\n1: Create notes\n2: Read notes\n3: Exit\n")
     if menu_choice == "1":
         os.system('clear')
